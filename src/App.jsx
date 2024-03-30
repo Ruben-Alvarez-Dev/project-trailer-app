eeb45f9cd8c08f6d012d317ba8df4ce25adbd1d8
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-03-30 13:05:23 +0100

    materialUI, fonts and icons installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => { 
   return (
     <>
       <h1>Movies App</h1>
     </>
   )
 }
-
-export default App
