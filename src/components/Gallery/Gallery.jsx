32446490a367e1cc793eb0338577dc2b5bc0cf4d
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-03-31 01:17:36 +0100

    snap scroll working

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,18 +3,18 @@ import poster from '../../assets/poster.jpg'
 import React from 'react'
 import { GalleryCollection } from './GalleryCollection'
 
-
-
-
-
 export const Gallery = () => {
   return (
     <div className="gallery">    
-    <div className="container">
-        <GalleryCollection title="Popular" />
-        <GalleryCollection title="Top Rated" />
-        <GalleryCollection title="Now Playing" />
-      </div>
+        <div className="galleryStart"></div>
+        <GalleryCollection className="galleryItem" title="One" />
+        <GalleryCollection className="galleryItem" title="Two" />
+        <GalleryCollection className="galleryItem" title="Three" />
+        <GalleryCollection className="galleryItem" title="Four" />
+        <GalleryCollection className="galleryItem" title="Five" />
+        <GalleryCollection className="galleryItem" title="Six" />
+        <div className="galleryEnd"></div>
     </div>
+    
   )
 }
